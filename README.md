f28e01f4daefa65faa99535a61f37399ab0e0b93
Kasen <18170166+imkasen@users.noreply.github.com>
2023-12-28 20:08:24 +0800

    update project structure

--- README.md
+++ README.md
@@ -1,13 +1,24 @@
 # Coqui.ai/TTS Demo
 
-模型下载地址：[.models.json](https://github.com/coqui-ai/TTS/blob/dev/TTS/.models.json)
-
-默认下载路径：
-
-- Linux：`~/.local/share/tts`
-- Windows：`C:\Users\<UserName>\AppData\Local\tts`
-
 ## 安装
 
 1. 从 Pytoch [官网](https://download.pytorch.org/whl/)下载对应版本的 torch、torchvision、torchaudio 等，或者 `pip install torch==2.1.0 torchvision==0.16.0 torchaudio==2.1.0 --index-url https://download.pytorch.org/whl/cu118`
 2. 创建虚拟环境，安装依赖：`pip install -r requirements.txt`
+
+## 模型
+
+模型信息：[.models.json](https://github.com/coqui-ai/TTS/blob/dev/TTS/.models.json)
+
+查看模型列表：
+
+``` Python
+tts --list_models
+tts --model_info_by_name "<model_type>/<language>/<dataset>/<model_name>"
+```
+
+### 默认下载路径
+
+- Linux：`~/.local/share/tts`
+- Windows：`C:\Users\<UserName>\AppData\Local\tts`
+
+具体实现详见 `TTS/utils/generic_utils.py` 中 `get_user_data_dir` 方法和 `TTS/utils/manage.py` 中 `ModelManager` 的 `__init__` 方法。
--- demo/gradio_demo.py
+++ demo/gradio_demo.py
@@ -1,10 +1,12 @@
 """
-利用 Gradio 构建 UI 操作界面
+利用 Gradio 构建基本 UI 操作界面
 """
 import os
 import sys
+from pathlib import Path
 
 import gradio as gr
+import numpy as np
 import torch
 from TTS.api import TTS
 
@@ -17,36 +19,48 @@ def run_tts(
 ) -> str:
     """
     运行 TTS
+
+    Gradio 中使用 tts() 输出音质似乎不如 tts_to_file()
     """
     device: str = "cuda" if torch.cuda.is_available() else "cpu"
-
-    # TODO
-    # Text to speech list of amplitude values as output
-    # return tts.tts(text=text, speaker_wav=speaker_wav_path, language=language)
+    tts: TTS
+    wav: list[int]
 
     match model_name:
         case "tts_models/multilingual/multi-dataset/xtts_v2":
             if sys.platform == "win32":
                 localappdata_path: str = os.getenv("LOCALAPPDATA")
-                model_path: str = os.path.join(
-                    localappdata_path,
-                    "tts",
-                    "tts_models--multilingual--multi-dataset--xtts_v2",
-                )
-                config_path: str = os.path.join(model_path, "config.json")
-                # 当前模型使用 model_path 加载要快于 model_name 的方式
-                tts: TTS = TTS(model_path=model_path, config_path=config_path).to(device)
-                tts.tts_to_file(text=text, speaker_wav=speaker_wav_path, language=language, file_path="output.wav")
+            elif sys.platform == "linux":
+                localappdata_path: str = Path.home().joinpath(".local/share")
+            model_path: str = os.path.join(
+                localappdata_path,
+                "tts",
+                "tts_models--multilingual--multi-dataset--xtts_v2",
+            )
+            config_path: str = os.path.join(model_path, "config.json")
+
+            # 当前多语言模型使用 model_path 加载要快于 model_name 的方式
+            if os.path.isdir(model_path):
+                tts = TTS(model_path=model_path, config_path=config_path).to(device)
+            else:
+                tts = TTS(model_name=model_name).to(device)
+
+            # tts.tts_to_file(text=text, speaker_wav=speaker_wav_path, language=language, file_path="output.wav")
+            wav = tts.tts(text=text, speaker_wav=speaker_wav_path, language=language)
+
         case "tts_models/zh-CN/baker/tacotron2-DDC-GST":
-            tts: TTS = TTS(model_name=model_name).to(device)
-            tts.tts_to_file(text=text, file_path="output.wav")
-    return "output.wav"
+            tts = TTS(model_name=model_name).to(device)
+            # tts.tts_to_file(text=text, file_path="output.wav")
+            wav = tts.tts(text=text)
+
+    # return "output.wav"
+    return tts.synthesizer.output_sample_rate, np.array(wav)
 
 
 if __name__ == "__main__":
     # Gradio 组件
     # Inputs
-    text_box = gr.Textbox(label="输入", placeholder="输入用于语音合成的文本内容")
+    text_box = gr.Textbox(label="输入", placeholder="输入用于语音合成的文本内容", lines=5)
     model_name_box = gr.Dropdown(
         label="模型",
         info="选择需要使用的模型",
@@ -55,13 +69,16 @@ if __name__ == "__main__":
             "tts_models/zh-CN/baker/tacotron2-DDC-GST",
         ],
     )
-    speaker_wav_box = gr.Audio(label="语言克隆文件", type="filepath")
+    speaker_wav_box = gr.Audio(label="语言克隆文件（可选）", type="filepath")
     language_box = gr.Radio(
-        label="语言",
+        label="语言（可选）",
         choices=["en", "zh"],
     )
     # Outputs
-    output_audio_box = gr.Audio(label="语言合成结果")
+    output_audio_box = gr.Audio(
+        label="语言合成结果",
+        autoplay=True,
+    )
 
     # UI 界面
     demo = gr.Interface(
@@ -74,6 +91,8 @@ if __name__ == "__main__":
         ],
         outputs=output_audio_box,
         allow_flagging="never",
-    )  # TODO: queue
+        title="Coqui.ai TTS Interface Demo",
+        article="_注：只有多语言模型需要**上传语音克隆文件**和**选择语言**。_",
+    ).queue()
 
     demo.launch(show_api=False)
